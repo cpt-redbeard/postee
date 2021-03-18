e4577a713ccacb67962dd5b94a1d3fdcfb72964a
pthomson <pat.thomson12@gmail.com>
2021-03-18 13:07:38 -0400

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 8082:8082 -p 8445:8445 -e AQUAALERT_CFG=/alert.yaml -v ~/upwork/aquasec/webhook-server/alert.yaml:/alert.yaml aquasec/postee-server
+docker run -d -p 8082:8082 -p 8445:8445 -e AQUAALERT_CFG=/alert.yaml -v ~/upwork/aquasec/webhook-server/alert.yaml:/alert.yaml aquasec/postee
